c941d26ae0c781770b34932f92ed9215c8e2ec19
Paweł Sierant <paw9810@gmail.com>
2021-03-23 13:50:50 +0100

    Added Header and Post component

---
+++ client/src/components/Header.js
@@ -0,0 +1,97 @@
+import React from "react";
+import { useState } from "react";
+import Box from "@material-ui/core/Box";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import Drawer from "@material-ui/core/Drawer";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import HomeIcon from "@material-ui/icons/Home";
+import AccountCircleIcon from "@material-ui/icons/AccountCircle";
+import AddCircleIcon from "@material-ui/icons/AddCircle";
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
+import VpnKeyIcon from "@material-ui/icons/VpnKey";
+
+const Header = ({ location }) => {
+  const [menuState, setMenuState] = useState(false);
+
+  const toggleDrawer = (open) => (event) => {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
+    setMenuState(open);
+  };
+
+  return (
+    <Box component="header">
+      <AppBar position="sticky">
+        <Toolbar>
+          <IconButton
+            onClick={toggleDrawer(true)}
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Drawer onClose={toggleDrawer(false)} anchor="left" open={menuState}>
+            <List>
+              <div
+                onClick={toggleDrawer(false)}
+                onKeyDown={toggleDrawer(false)}
+              >
+                <ListItem button>
+                  <ListItemIcon>
+                    <HomeIcon />
+                  </ListItemIcon>
+                  <ListItemText primary="Home" />
+                </ListItem>
+                <ListItem button>
+                  <ListItemIcon>
+                    <VpnKeyIcon />
+                  </ListItemIcon>
+                  <ListItemText primary="Login" />
+                </ListItem>
+                <ListItem button>
+                  <ListItemIcon>
+                    <VpnKeyIcon />
+                  </ListItemIcon>
+                  <ListItemText primary="Register" />
+                </ListItem>
+                <ListItem button>
+                  <ListItemIcon>
+                    <AccountCircleIcon />
+                  </ListItemIcon>
+                  <ListItemText primary="Profile" />
+                </ListItem>
+                <ListItem button>
+                  <ListItemIcon>
+                    <AddCircleIcon />
+                  </ListItemIcon>
+                  <ListItemText primary="Add post" />
+                </ListItem>
+                <ListItem button>
+                  <ListItemIcon>
+                    <ExitToAppIcon />
+                  </ListItemIcon>
+                  <ListItemText primary="Logout" />
+                </ListItem>
+              </div>
+            </List>
+          </Drawer>
+          <Typography variant="h6">{location}</Typography>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+};
+
+export default Header;
---
+++ client/src/components/Post.js
@@ -0,0 +1,74 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Box from "@material-ui/core/Box";
+import Container from "@material-ui/core/Container";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import ButtonGroup from "@material-ui/core/ButtonGroup";
+import Grid from "@material-ui/core/Grid";
+import ThumbUpIcon from "@material-ui/icons/ThumbUp";
+import ThumbDownIcon from "@material-ui/icons/ThumbDown";
+
+//test image
+import image from "../img/testimage.jpg";
+
+const useStyles = makeStyles({
+  media: {
+    minHeight: 250,
+    backgroundSize: "contain",
+  },
+  desc: {
+    paddingTop: 0,
+  },
+});
+
+const Post = () => {
+  const classes = useStyles();
+
+  return (
+    <Box>
+      <Container maxWidth="sm">
+        <Card>
+          <CardActions>
+            <Button size="medium" color="primary">
+              Title
+            </Button>
+          </CardActions>
+          <CardContent className={classes.desc}>
+            <Typography variant="body2" color="textSecondary" component="p">
+              tagi tagi tagi tagi
+            </Typography>
+          </CardContent>
+          <CardActionArea>
+            {/* test image */}
+            <CardMedia className={classes.media} image={image} title="metin" />
+          </CardActionArea>
+          <CardActions>
+            <Grid container justify="center">
+              <ButtonGroup
+                variant="contained"
+                color="primary"
+                aria-label="contained primary button group"
+              >
+                <Button>
+                  <ThumbUpIcon />
+                </Button>
+                <Button variant="text">450</Button>
+                <Button color="secondary">
+                  <ThumbDownIcon />
+                </Button>
+              </ButtonGroup>
+            </Grid>
+          </CardActions>
+        </Card>
+      </Container>
+    </Box>
+  );
+};
+
+export default Post;
